fb2ca7d48748b77d5b7c905d329ab809dbffc42b
Bruno Martins <bgcngm@gmail.com>
2023-10-25 14:55:35 +0100

    m5_tab: Add proper skeleton
    This way EPPE works!
    
    Change-Id: Ib3cd7df620c1f5235114a1fe2ca078cbf6ed2c48

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_m5_tab.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_m5_tab-eng \
+    lineage_m5_tab-user \
+    lineage_m5_tab-userdebug
